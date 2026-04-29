fce29ad2896d8c041dfb2fa401db163e9b2e7260
Sahrin Tahiyat Choudhury <144997115+Sahrin-Tahiyat-Choudhury@users.noreply.github.com>
2026-04-30 04:49:39 +0530

    Delete BACKEND directory

--- BACKEND/src/db/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-async function connectDB() {
-    await mongoose.connect("mongodb://yt:bQlVsX9MDlZoY0Ld@ac-8jrpd3t-shard-00-00.chlzwe6.mongodb.net:27017,ac-8jrpd3t-shard-00-01.chlzwe6.mongodb.net:27017,ac-8jrpd3t-shard-00-02.chlzwe6.mongodb.net:27017/?ssl=true&replicaSet=atlas-112c2o-shard-0&authSource=admin&appName=yt-complete-backend/halley")
-    
-    console.log("Connected to DB")
-}
-
-module.exports = connectDB
--- BACKEND/src/routes/wishlistRoutes.js
+++
@@ -1,4 +0,0 @@
-const express = require('express');
-const router = express.Router()
-const wishlistModel = require("../models/wishlist.model")
-module.exports = router;
