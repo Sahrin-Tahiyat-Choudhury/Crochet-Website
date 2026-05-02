f6de439587e4decb613327b138b4abbdb0cf3827
srehh <sanchitachakraborty102005@gmail.com>
2026-05-02 22:06:33 +0530

    Delete Frontend/Yarn- journey/script.js

--- Frontend/Yarn- journey/script.js
+++
@@ -1,4 +0,0 @@
-function showPanel(name) {
-  document.getElementById('panel-signin').style.display = name === 'signin' ? 'block' : 'none';
-  document.getElementById('panel-create').style.display = name === 'create' ? 'block' : 'none';
-}
