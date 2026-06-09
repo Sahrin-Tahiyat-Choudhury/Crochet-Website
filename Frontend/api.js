7dbe8a234d06f03b1894ed35be73e342ff0b690f
Sahrin Tahiyat Choudhury <144997115+Sahrin-Tahiyat-Choudhury@users.noreply.github.com>
2026-06-09 21:16:38 +0530

    render website connection

--- Frontend/api.js
+++ Frontend/api.js
@@ -1,5 +1,8 @@
 const API = (() => {
-    const BASE = 'http://127.0.0.1:3000/api'; 
+    const BASE = window.location.hostname === 'localhost' || 
+             window.location.hostname === '127.0.0.1'
+  ? 'http://127.0.0.1:3000/api'
+  : 'https://crochet-website-7n9s.onrender.com/api';
 
     async function request(method, path, body = null, isFormData = false) {
         const options = {
